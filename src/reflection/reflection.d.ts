d20720158a7c58c776b7e9aad33816c20f96bbd1
yeerkkiller1 <yeerkkiller1@gmail.com>
2018-07-10 16:14:12 -0400

    Global Omit type.

--- src/reflection/reflection.d.ts
+++ src/reflection/reflection.d.ts
@@ -1,3 +1,6 @@
+// https://stackoverflow.com/questions/41476063/typescript-remove-key-from-type-subtraction-type
+type Omit<O, D extends string> = Pick<O, Exclude<keyof O, D>>;
+
 declare namespace Types { 
     type ErasedType = string&number;
 
