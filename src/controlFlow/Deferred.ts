ad3c9eec50c0e539f4629351773f1256af28ea60
yeerkkiller1 <yeerkkiller1@gmail.com>
2018-07-21 02:10:22 -0400

    Deferred should provide utilities for synchronous value accesses.

--- src/controlFlow/Deferred.ts
+++ src/controlFlow/Deferred.ts
@@ -1,4 +1,6 @@
 export class Deferred<T> {
+    private value: { v: T }|{error: any}|undefined;
+
     private resolveInternal!: (value: T) => void;
     private rejectInternal!: (error: any) => void;
     private promise = new Promise<T>((resolve, reject) => {
@@ -7,14 +9,20 @@ export class Deferred<T> {
     });
 
     public Resolve(value: T): this {
+        this.value = this.value || { v: value };
         this.resolveInternal(value);
         return this;
     }
     public Reject(error: any): this {
+        this.value = this.value || { error: error };
         this.rejectInternal(error);
         return this;
     }
     public Promise() {
         return this.promise;
+    }
+
+    public Value() {
+        return this.value;
     }
 }
